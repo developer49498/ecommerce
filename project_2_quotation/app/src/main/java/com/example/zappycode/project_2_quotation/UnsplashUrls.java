7c49a695b3fafda83646e8592140d022139fc7cc
developer49498 <dynamicprogramming49@gmail.com>
2025-08-04 18:04:13 +0530

    Delete project_2_quotation directory

--- project_2_quotation/app/src/main/java/com/example/zappycode/project_2_quotation/UnsplashUrls.java
+++
@@ -1,9 +0,0 @@
-package com.example.zappycode.project_2_quotation;
-
-public class UnsplashUrls {
-    private String regular; // Image URL
-
-    public String getRegular() {
-        return regular;
-    }
-}
